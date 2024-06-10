31b7d48765eacb3d7eab8a9092d60c652ef82d92
TomJMiddleton <tom@middletonsonline.co.uk>
2024-06-10 22:43:45 +0100

    testing LDA-NG

--- Scripts/TopicAnalysis.py
+++ Scripts/TopicAnalysis.py
@@ -1,29 +1,43 @@
 ### Thomas Middleton
-
+from NewsGroupPreprocessing import GetNewsFileNames, RemoveNewsFormatting
+import string 
 from gensim.models import LdaModel
 from gensim.corpora import Dictionary
-
-# Example corpus
-documents = [
-    "Basketball is a popular sport.",
-    "Soccer players can score goals.",
-    "Tennis is played with a racket.",
-    "Basketball players score points by shooting the ball through the hoop.",
-    "Soccer is known as football in many countries."
-]
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
 
 # Preprocess the text
-texts = [[word for word in document.lower().split()] for document in documents]
+def TokeniseDoc(document):
+    tokens = [word for word in document.lower().split() if word not in STOPWORDS]
+    clean_tokens = []
+    for token in tokens:
+        clean_token = token.translate(punct_table).strip()
+        if clean_token: clean_tokens.append(clean_token)
+    return clean_tokens
 
-# Create a dictionary representation of the documents
-dictionary = Dictionary(texts)
+def ProcessChunk(docs):
+    tokenised_texts = [TokeniseDoc(doc) for doc in docs]
+    dictionary.add_documents(tokenised_texts)
+    return [dictionary.doc2bow(text) for text in tokenised_texts]
 
-# Convert dictionary to a bag of words corpus
-corpus = [dictionary.doc2bow(text) for text in texts]
+def NERForNewsGroup(NUMTOPICS):
+    db_filenames = GetNewsFileNames()
+    unformatted_text = RemoveNewsFormatting(db_filenames)
+    corpus = ProcessChunk(unformatted_text)
+    print("\n Data Imported \n")
+    lda = LdaModel(corpus=corpus, num_topics=NUMTOPICS, id2word=dictionary, passes=15)
+    print("\n LDA completed \n")
+    return lda
 
-# Train the LDA model
-lda = LdaModel(corpus, num_topics=3, id2word=dictionary, passes=15)
+punct_table = str.maketrans('', '', string.punctuation)
+NUMTOPICS = 20
+STOPWORDS = set(stopwords.words('english'))
+#print(STOPWORDS)
+
+dictionary = Dictionary()
+lda = NERForNewsGroup(NUMTOPICS)
 
 # Print the topics
-for i, topic in lda.print_topics(num_topics=3, num_words=3):
+for i, topic in lda.print_topics(num_topics=NUMTOPICS, num_words=3):
     print(f"Topic {i}: {topic}")
