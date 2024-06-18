3811ae2b697d540ae92c2a331a1d4816526c6dda
TomJMiddleton <tom@middletonsonline.co.uk>
2024-06-19 00:56:16 +0100

    added document topics: write to csv

--- Scripts/TopicAnalysis.py
+++ Scripts/TopicAnalysis.py
@@ -1,6 +1,7 @@
 ### Thomas Middleton
-from NewsGroupPreprocessing import GetNewssave_file_names, RemoveNewsFormatting
+from NewsGroupPreprocessing import GetNewsFileNames, RemoveNewsFormatting
 import string 
+import csv
 from gensim.corpora import MmCorpus
 from gensim.models import LdaModel
 from gensim.corpora import Dictionary
@@ -28,7 +29,7 @@ def ExportLDAModel(path, name):
     # Save the model, dictionary, corpus
     lda.save(path + name + '_lda_model.gensim')
     dictionary.save(path + name + '_lda_dictionary.gensim')
-    MmCorpus.serialize(path + name + 'lda_corpus.mm', corpus)
+    MmCorpus.serialize(path + name + '_lda_corpus.mm', corpus)
 
 def ImportLDAModel(path, name):
     # Load the model, dictionary, corpus
@@ -38,7 +39,7 @@ def ImportLDAModel(path, name):
     return lda_loaded, dictionary_loaded, corpus_loaded
 
 def NERForNewsGroup(NUMTOPICS):
-    db_save_file_names = GetNewssave_file_names()
+    db_save_file_names = GetNewsFileNames()
     unformatted_text = RemoveNewsFormatting(db_save_file_names)
     corpus = ProcessChunk(unformatted_text)
     print("\n Data Imported \n")
@@ -50,10 +51,35 @@ def PrintTopics(lda, n_words):
     for i, topic in lda.print_topics(num_topics=NUMTOPICS, num_words=n_words):
         print(f"Topic {i}: {topic}")
 
+def GetDocumentTopics(document, n_topics = 2):
+    tokens = TokeniseDoc(document)
+    bag_of_words = dictionary.doc2bow(tokens)
+    topic_distribution = lda.get_document_topics(bag_of_words)
+    sorted_topics = sorted(topic_distribution, key=lambda x: x[1], reverse=True)
+    sorted_topics = sorted_topics[:n_topics]
+    return sorted_topics
+
+def WriteNGTopics(n_topics):
+    db_save_file_names = GetNewsFileNames()
+    unformatted_text = RemoveNewsFormatting(db_save_file_names)
+    save_file_path = "Datasets/Processed/20NG/"
+    print("Documents Imported")
+    with open(save_file_path + 'test_topics.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        for doc in unformatted_text:
+            topics = GetDocumentTopics(doc, )
+            doc_topics = [ITEM for pair in topics for ITEM in pair]
+            writer.writerow(doc_topics, n_topics)
+    print(f" Written {n_topics} topics per document to csv")
+
+
+
 # CONSTANTS -----------------------------------------------------------------------------
 NBELOW = 12
 NOVER = 0.3
 NKEEP = 60000
+
+NTOPICSTOWRITE = 2
 
 NUMTOPICS = 20
 STOPWORDS = set(stopwords.words('english'))
@@ -66,8 +92,10 @@ save_file_name = "TestOne"
 
 # Code ----------------------------------------------------------------------------------
 
-lda, corpus = NERForNewsGroup(NUMTOPICS)
+#lda, corpus = NERForNewsGroup(NUMTOPICS)
 
-ExportLDAModel(save_file_path, save_file_name)
+#ExportLDAModel(save_file_path, save_file_name)
 
 lda, dictionary, corpus = ImportLDAModel(save_file_path, save_file_name)
+
+WriteNGTopics(NTOPICSTOWRITE)
