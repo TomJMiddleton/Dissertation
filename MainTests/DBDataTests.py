fed86b37e5b987a2940f2cd3b4013c6686ea555d
TomJMiddleton <tom@middletonsonline.co.uk>
2024-07-31 23:28:10 +0100

    added sqlite security

--- MainTests/DBDataTests.py
+++ MainTests/DBDataTests.py
@@ -1,28 +1,20 @@
 import sqlite3
+from contextlib import closing
 
+
+# Export n sentences from the Sentence table to a text file
 def ReviewSentenceTableInTxtFile(db_path, output_file, n):
-    # Connect to the SQLite database
-    conn = sqlite3.connect(db_path)
-    cur = conn.cursor()
-    
-    # Execute the query to get the first n sentences
-    cur.execute("SELECT sent FROM Sentence LIMIT ?", (n,))
-    
-    # Fetch all results
-    sentences = cur.fetchall()
-    
-    # Write sentences to the output file
-    with open(output_file, 'w', encoding='utf-8') as f:
-        for sentence in sentences:
-            f.write(sentence[0] + '\n')
-    
-    # Close the database connection
-    conn.close()
-    
+    with closing(sqlite3.connect(db_path)) as conn:
+        with closing(conn.cursor()) as cur:
+            cur.execute("SELECT sent FROM Sentence LIMIT ?", (n,))
+            sentences = cur.fetchall()
+            with open(output_file, 'w', encoding='utf-8') as f:
+                for sentence in sentences:
+                    f.write(sentence[0] + '\n')
     print(f"{len(sentences)} sentences have been written to {output_file}")
 
 
-
+# NG sentence processing visual check
 ReviewSentenceTableInTxtFile(db_path='./Datasets/Database/NewsGroupDB.db',
                              output_file = 'db_export_sentences.txt',
                              n = 3000)
